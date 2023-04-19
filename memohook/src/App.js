7f76fdd905b88775d7e65f66ecb7cb4024a6b8d8
youngw77 <103637293+youngw77@users.noreply.github.com>
2023-04-19 17:36:42 +0900

    useMemo 사용

--- memohook/src/App.js
+++ memohook/src/App.js
@@ -1,24 +1,42 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 
 const hardCalculate = (number) => {
-  console.log('계산');
-  for(let i=0; i<99999999; i++){}
+  console.log('어려운계산');
+  for(let i=0; i<99999999; i++){} // 생각하는 시간
   return number + 10000;
+}
+
+const easyCalculate = (number) => {
+  return number + 1;
 }
 
 function App() {
   const [hardNumber, setHardNumber] = useState(1);
-  const hardSum = hardCalculate(hardNumber);
+  const [easyNumber, setEasyNumber] = useState(1);
+
+  // const hardSum = hardCalculate(hardNumber);
+  const hardSum = useMemo(() => {
+    return hardCalculate(hardNumber);
+  }, [hardNumber]);
+  const easySum = easyCalculate(easyNumber);
 
   return (
     <div>
-      <h3>계산기</h3>
+      <h3>어려운 계산기</h3>
       <input
       type="number"
       value={hardNumber}
       onChange={(e) => setHardNumber(parseInt(e.target.value))}
       />
       <span> + 10000 = {hardSum}</span>
+
+      <h3>쉬운 계산기</h3>
+      <input
+      type="number"
+      value={easyNumber}
+      onChange={(e) => setEasyNumber(parseInt(e.target.value))}
+      />
+      <span> + 1 = {easySum}</span>
     </div>
 
   );
